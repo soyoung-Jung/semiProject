7d831eaa39b9ec5354659c3d7ec22bdc151dc3e9
soyoung-Jung <so_young_jung@naver.com>
2020-08-14 15:41:18 +0900

    git 기능확인용

---
+++ src/view/ProductListView.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Grid} from 'semantic-ui-react'
+
+
+class ProductListView extends Component {
+    render() {
+        return (
+            <Grid>
+                <Grid.Row columns={4}>
+                <Grid.Column>
+
+                </Grid.Column>
+                <Grid.Column>
+
+                </Grid.Column>
+                <Grid.Column>
+
+                </Grid.Column>
+                <Grid.Column>
+
+                </Grid.Column>
+                </Grid.Row>
+            </Grid>
+        );
+    }
+}
+
+export default ProductListView;
