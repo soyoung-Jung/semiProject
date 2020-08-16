1740e7d8b3cef61bc716812f2e58c45c6385c76f
KKJ620 <kyungju620@gmail.com>
2020-08-16 15:36:38 +0900

    show how to merge

--- src/view/FooterView.js
+++ src/view/FooterView.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import { Grid} from 'semantic-ui-react'
+import { Grid, Header} from 'semantic-ui-react'
 
 class FooterView extends Component {
+    //test 
     render() {
         return (
             <Grid columns={3} divided>
                 <Grid.Row>
-                    <Grid.Column> 
+                    <Grid.Column>
+                        <Header as="h1">고객센터</Header>
                     </Grid.Column>      
                 </Grid.Row>
                 <Grid.Row>
