7da2c8628676df48db453b400cf771f8b6630f93
seeker1207 <jm3420@naver.com>
2020-08-17 22:14:26 +0900

    스토어 수정 (ver2)

--- src/store/UserStore.js
+++ src/store/UserStore.js
@@ -2,19 +2,66 @@ import { observable, computed, action } from "mobx";
 import generateId from "../IDGenerator";
 
 class UserStore{
-    //유저목록
+    //회원목록
     @observable
     users = [
         {
             id : generateId(5),
             name : "손지훈",
+            userID : "msaclass",
+            userPassword : "12345",
+        },
+        {
+            id : generateId(5),
+            name : "김경주",
+            userID : "",
+            userPassword : "",
+        },
+        {
+            id : generateId(5),
+            name : "정소영",
+            userID : "",
+            userPassword : "",
+        },
+        {
+            id : generateId(5),
+            name : "여민호",
             userID : "",
             userPassword : "",
         }
     ]
 
+    @observable
+    user = {
 
-
+    }
+    
+    //회원추가
+    @action
+    addProduct(user){
+        this.users.add(user);
+    }
+    
+    //회원제거
+    @action
+    removeProduct(selectedId){
+        this.users = this.users.filter((user) => user.id !== selectedId);
+    }
+    
+    @action 
+    modifyTodo(selectedProduct) {
+        //상품리스트에서 선택된 상품의 아이디와 같은 상품객체 수정
+        this.users = this.users.map((todo) =>
+        todo.id === selectedProduct.id ? selectedProduct : todo
+        );
+        console.log(this.users);
+    }
+    
+    @action
+    selectTodo(selectedId) {
+        //상품리스트에서 id가 같은 상품 객체 리턴
+        this.user = this.users.find((user) => user.id === selectedId);
+    }
 
 }
 
