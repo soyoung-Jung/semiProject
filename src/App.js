22dcc0df85503418c6ef735e370436ae26839b42
seeker1207 <jm3420@naver.com>
2020-08-18 10:57:01 +0900

    스토어 메인페이지 상품목록

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React from 'react';
+
+import './App.css';//hi
+import { Grid, Image } from 'semantic-ui-react'
+import HeaderView from './view/HeaderView';
+import ProductListView from './view/ProductListView';
+import ProductDetail from './view/ProductDetail';
+
+
+function App() {
+  return (
+      <>
+    <Grid divided='vertically'>
+      <Grid.Row centered
+ columns={1}>
+        <HeaderView />
+      </Grid.Row>
+    </Grid>
+    <Grid columns={1}>
+    <Grid.Row>
+      <Grid.Column>
+
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='/images/wireframe/paragraph.png' />
+      </Grid.Column>
+      <Grid.Column>
+        <Image src='/images/wireframe/paragraph.png' />
+      </Grid.Column>
+    </Grid.Row>
+
+
+      <Grid.Row columns={1}>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+      </Grid.Row>
+      
+      <Grid.Row columns={1}>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+      </Grid.Row>
+
+      <Grid.Row columns={1}>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+        <Grid.Column>
+          <Image src='/images/wireframe/paragraph.png' />
+        </Grid.Column>
+      </Grid.Row>
+      
+    </Grid>
+    </>
+  );
+}
+
+export default App;
