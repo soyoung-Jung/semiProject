efa6eeba02b9a0416f089d77d97f9c5f0951cd6c
KKJ620 <kyungju620@gmail.com>
2020-08-18 13:30:41 +0900

    한시삼십분

--- src/view/HeaderView.js
+++ src/view/HeaderView.js
@@ -1,22 +1,54 @@
-import React, { Component } from 'react';
-import {Form, Button, Icon, Input, Menu} from 'semantic-ui-react';
+import React from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import "./HeaderView.css"
-class HeaderView extends Component {    
-    
-    render() {
-        return (
-            <Form classNmae="form">
-                <Menu fixed="top"><Button icon size='huge'><Icon name='bars' /></Button></Menu>
-              
+import "./HeaderView.css";
+import { Button, Form, Modal, Icon, Menu, Input } from "semantic-ui-react";
+
+function HeaderView() {
+    const [open, setOpen] = React.useState(false)
+  
+    return (
+        <Form classNmae="form">
+            <Menu fixed="top">
+                <Button icon size='huge'><Icon name='bars' /></Button>
                 <Icon size="huge" name="bath"/>
                 <Input size='huge' icon='search' placeholder='Search...' />
-                <Button icon size='huge'><Icon name= 'user circle outline'/></Button>
-                <Button icon size='huge'><Icon name ='shopping cart' /></Button>
-              
-            </Form>
-          );
-    }
-}
-
+                <Modal
+                    onClose={() => setOpen(false)}
+                    onOpen={() => setOpen(true)}
+                    open={open}
+                    trigger={<Button icon size='huge'><Icon name= 'user circle outline'/>LOGIN</Button>}    >
+                    <Modal.Header>Login Page</Modal.Header>
+                    <Modal.Content form>
+                        <Form.Input
+                            icon='user'
+                            iconPosition='left'
+                            label='Username'
+                            placeholder='Username'
+                        />
+                        <Form.Input
+                            icon='lock'
+                            iconPosition='left'
+                            label='Password'
+                            type='password'
+                            placeholder='Password'
+                        />
+                        <Button content='Login' primary />
+                    </Modal.Content>
+                    <Modal.Actions>
+                    <Button color='black' onClick={() => setOpen(false)}>
+                        Sign Up
+                    </Button>
+                    <Button
+                        content="Login"
+                        labelPosition='right'
+                        icon='checkmark'
+                        onClick={() => setOpen(false)}
+                        positive
+                    />
+                    </Modal.Actions>
+                </Modal>
+            </Menu>
+        </Form>
+    )
+  }
 export default HeaderView;
