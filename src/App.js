7e454e10e6e34b89447a86b3ccdb628cc2760ee2
KKJ620 <kyungju620@gmail.com>
2020-08-18 16:06:15 +0900

    4시6분

--- src/App.js
+++ src/App.js
@@ -3,33 +3,21 @@ import { Grid } from 'semantic-ui-react'
 import HeaderView from './view/HeaderView';
 import FooterView from './view/FooterView';
 import PromotionView from './view/PromotionView';
-import LoginView from './view/LoginView';
 
 
 function App() {
   return (
     <Grid divided='vertically'>
-      <Grid.Row centered
-      columns={1}>
+      <Grid.Row centered columns={1}>
         <HeaderView />
       </Grid.Row>
-   
-      <Grid.Row centered
-      columns={1}>
+      <Grid.Row centered columns={1}>
         <PromotionView />
       </Grid.Row>
-
-      <Grid.Row centered
-      columns={1}>
-        <LoginView />
-      </Grid.Row>
-      
-
-      <Grid.Row centered
-      columns={1}>
+      <Grid.Row centered columns={1}>
         <FooterView />
       </Grid.Row>
-      
+
     </Grid>
   );
 }
